5110a4a2ed96a0fc8b1d6b4f97c58d66f1a94e1d
vitoriapguimaraes <vpistoriguimaraes@gmail.com>
2025-09-18 18:14:58 -0300

    add prescription list on home + padding all pages

--- MediApp/medi-app/src/app/fake/page.tsx
+++ MediApp/medi-app/src/app/fake/page.tsx
@@ -1,36 +1,59 @@
-"use client"
+"use client";
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
+import Link from "next/link";
 
 export default function FakeApi() {
+  const [fakeApiData, setFakeApiData] = useState<any[]>([]);
 
-    const [fakeApiData, setFakeApiData] = useState(null);
-
-    const requestFakeApi = async () => {
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-            setFakeApiData(response.data);
-        } catch (e) {
-            console.log(e);
-        }
-
+  const requestFakeApi = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setFakeApiData(response.data);
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    return (
-        <>
-            <h1>Dados Fake Api</h1>
-            <button className="bg-green-500 p-2 inline-block text-white text-sm"
-             onClick={(e) => requestFakeApi()}>Buscar dados Fake Api</button>
-            <table>
-                {!!fakeApiData && fakeApiData.map((data: any) => (
-                    <tr>
-                        <td className='border border-slate-300'>{data.name}</td>
-                        <td className='border border-slate-300 text-center'>{data.username}</td>
-                        <td className='border border-slate-300 text-center'>{data.email}</td>
-                        <td className='border border-slate-300 text-center'>{data.phone}</td>
-                    </tr>
-                ))}
-            </table>
-        </>
-    )
+  return (
+    <>
+      <div className="flex-col items-center p-7 rounded-2xl">
+        <Link
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          href="/home"
+        >
+          Voltar
+        </Link>
+        <br></br>
+        <br></br>
+        <button
+          className="bg-green-500 p-2 inline-block text-white text-sm"
+          onClick={(e) => requestFakeApi()}
+        >
+          Buscar dados Fake Api
+        </button>
+        <br></br>
+        <br></br>
+        <table>
+          {!!fakeApiData &&
+            fakeApiData.map((data: any) => (
+              <tr>
+                <td className="border border-slate-300">{data.name}</td>
+                <td className="border border-slate-300 text-center">
+                  {data.username}
+                </td>
+                <td className="border border-slate-300 text-center">
+                  {data.email}
+                </td>
+                <td className="border border-slate-300 text-center">
+                  {data.phone}
+                </td>
+              </tr>
+            ))}
+        </table>
+      </div>
+    </>
+  );
 }
--- MediApp/medi-app/src/app/layout.tsx
+++ MediApp/medi-app/src/app/layout.tsx
@@ -1,28 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Header from "./components/Headers";
-import './globals.css'
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Medi App',
-  description: 'Medical application',
-}
+  title: "Medi-app",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <div className='w-full max-w-7xl mt-4 m-auto'>
-            {children}
-        </div>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
-  )
+  );
 }
